82129274df6532cda2f585118de55a4965fe5626
extendedstudio <insidecore@naver.com>
2026-05-22 01:43:07 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAbRRZfWmS3n8kYUL8AvHfaxQXuQf018H8",
+  apiKey: "AIzaSyBjLF36OWYOZ-xefFMujXryn7XeG9C5lYO",
   authDomain: "extended-studio.firebaseapp.com",
   projectId: "extended-studio",
   storageBucket: "extended-studio.firebasestorage.app",
-  messagingSenderId: "563838618066",
-  appId: "1:563838618066:web:ca574ede1c4524b5d3e00b"
+  messagingSenderId: "910732755063",
+  appId: "1:910732755063:web:6f3d3655ff691610920672",
+  measurementId: "G-ZKMVPJV6TQ"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
